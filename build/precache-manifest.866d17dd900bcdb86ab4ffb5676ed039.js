a7ca6009ae29b726021b3577a64a0acdbed6a8d4
bharat kumar shah <akashche72@gmail.com>
2021-03-01 13:34:52 +0400

    all files is added

---
+++ build/precache-manifest.866d17dd900bcdb86ab4ffb5676ed039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0b7399642944d7267462c51e7cca91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a19fd0dc4c6bb62df63",
+    "url": "/static/css/main.254c13fe.chunk.css"
+  },
+  {
+    "revision": "1a3913fbc02bd890e731",
+    "url": "/static/js/2.7ce09311.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7ce09311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a19fd0dc4c6bb62df63",
+    "url": "/static/js/main.ae6c9cb5.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './stylesheet/styles.css';
+import App from './App';
+import { debugContextDevtool } from 'react-context-devtool';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
+
+const options = {};
+debugContextDevtool(document.getElementById('root'), options);
